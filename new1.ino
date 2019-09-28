18b0f3976e0f22b6500c3d0ccc5272f7aa727681
Dinakaranevil <48046561+Dinakaranevil@users.noreply.github.com>
2019-09-28 16:02:24 +0530

    Add files via upload

---
+++ new1.ino
@@ -0,0 +1,103 @@
+// Example testing sketch for various DHT humidity/temperature sensors
+// Written by ladyada, public domain
+
+// REQUIRES the following Arduino libraries:
+// - DHT Sensor Library: https://github.com/adafruit/DHT-sensor-library
+// - Adafruit Unified Sensor Lib: https://github.com/adafruit/Adafruit_Sensor
+
+#include "DHT.h"
+
+#define DHTPIN 13 // Digital pin connected to the DHT sensor
+// Feather HUZZAH ESP8266 note: use pins 3, 4, 5, 12, 13 or 14 --
+// Pin 15 can work but DHT must be disconnected during program upload.
+
+// Uncomment whatever type you're using!
+//#define DHTTYPE DHT11   // DHT 11
+#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+
+// Connect pin 1 (on the left) of the sensor to +5V
+// NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
+// to 3.3V instead of 5V!
+// Connect pin 2 of the sensor to whatever your DHTPIN is
+// Connect pin 4 (on the right) of the sensor to GROUND
+// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
+
+// Initialize DHT sensor.
+// Note that older versions of this library took an optional third parameter to
+// tweak the timings for faster processors.  This parameter is no longer needed
+// as the current DHT reading algorithm adjusts itself to work on faster procs.
+DHT dht(DHTPIN, DHTTYPE);
+//sensor input PIN
+int mqInput = 26;
+//pull-down resistor value
+int mqR = 22000;
+//rO sensor value
+long rO = 41763;
+//min value for Rs/Ro
+float minRsRo = 0.358;
+//max value for Rs/Ro
+float maxRsRo = 2.428;
+//sensor a coefficient value
+float a = 116.6020682;
+//sensor b coefficient value
+float b = -2.769034857;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(mqInput, INPUT);
+  Serial.println(F("DHTxx test!"));
+
+  dht.begin();
+}
+
+void loop() {
+  // Wait a few seconds between measurements.
+  delay(2000);
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+  // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.print(F("°C "));
+  Serial.print(f);
+  Serial.print(F("°F  Heat index: "));
+  Serial.print(hic);
+  Serial.print(F("°C "));
+  Serial.print(hif);
+  Serial.println(F("°F"));
+  int adcRaw = analogRead(mqInput);
+  long rS = ((1024.0 * mqR) / adcRaw) - mqR;
+  Serial.print("Rs: ");
+  Serial.println(rS);
+  float rSrO = (float)rS / (float)rO;
+  Serial.print("Rs/Ro: ");
+  Serial.println(rSrO);
+  if(rSrO < maxRsRo && rSrO > minRsRo) {
+ float ppm = a * pow((float)rS / (float)rO, b);
+ Serial.print("ppm: ");
+ Serial.println(ppm);
+  } else {
+ Serial.println("Out of range.");
+  }
+  delay(1000);
+}
